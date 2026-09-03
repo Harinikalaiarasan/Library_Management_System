bec0db4df369a964c78ca640f295465dc49b9271
Harini-K123 <hariniarasan236@gmail.com>
2026-09-03 12:29:27 +0530

    Initial commit - Library Management System

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Book {
+
+    int bookId;
+    String title;
+    String author;
+    boolean issued;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.issued = false;
+    }
+
+    public void displayBook() {
+        System.out.println("Book ID: " + bookId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Status: " + (issued ? "Issued" : "Available"));
+        System.out.println("----------------------");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,173 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        while (true) {
+
+            System.out.println("\n===== LIBRARY MANAGEMENT SYSTEM =====");
+            System.out.println("1. Add Book");
+            System.out.println("2. View Books");
+            System.out.println("3. Search Book by ID");
+            System.out.println("4. Issue Book");
+            System.out.println("5. Return Book");
+            System.out.println("6. Delete Book");
+            System.out.println("7. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter Book ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter Book Title: ");
+                    String title = sc.nextLine();
+
+                    System.out.print("Enter Author Name: ");
+                    String author = sc.nextLine();
+
+                    books.add(new Book(id, title, author));
+
+                    System.out.println("Book added successfully!");
+                    break;
+
+                case 2:
+
+                    if (books.isEmpty()) {
+                        System.out.println("No books available.");
+                    } else {
+                        for (Book book : books) {
+                            book.displayBook();
+                        }
+                    }
+
+                    break;
+
+                case 3:
+
+                    System.out.print("Enter Book ID: ");
+                    id = sc.nextInt();
+
+                    boolean found = false;
+
+                    for (Book book : books) {
+
+                        if (book.bookId == id) {
+                            book.displayBook();
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Book not found!");
+                    }
+
+                    break;
+
+                case 4:
+
+                    System.out.print("Enter Book ID: ");
+                    id = sc.nextInt();
+
+                    found = false;
+
+                    for (Book book : books) {
+
+                        if (book.bookId == id) {
+
+                            found = true;
+
+                            if (book.issued) {
+                                System.out.println("Book is already issued!");
+                            } else {
+                                book.issued = true;
+                                System.out.println("Book issued successfully!");
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Book not found!");
+                    }
+
+                    break;
+
+                case 5:
+
+                    System.out.print("Enter Book ID: ");
+                    id = sc.nextInt();
+
+                    found = false;
+
+                    for (Book book : books) {
+
+                        if (book.bookId == id) {
+
+                            found = true;
+
+                            if (!book.issued) {
+                                System.out.println("Book is already available!");
+                            } else {
+                                book.issued = false;
+                                System.out.println("Book returned successfully!");
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Book not found!");
+                    }
+
+                    break;
+
+                case 6:
+
+                    System.out.print("Enter Book ID: ");
+                    id = sc.nextInt();
+
+                    found = false;
+
+                    for (Book book : books) {
+
+                        if (book.bookId == id) {
+                            books.remove(book);
+                            System.out.println("Book deleted successfully!");
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Book not found!");
+                    }
+
+                    break;
+
+                case 7:
+
+                    System.out.println("Thank you!");
+                    sc.close();
+                    return;
+
+                default:
+
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
 }
